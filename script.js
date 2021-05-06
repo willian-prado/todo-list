8c219510a93ff370b92c05475b6584ac9af447d9
Willian Prado <willian_prado@hotmail.com>
2021-05-06 20:36:44 -0300

    Resolve os requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const buttonCriarTarefa = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 
+function addBackground(event) {
+  const marked = document.querySelector('.marked');
+
+  if (marked) {
+    marked.classList.remove('marked');
+    event.target.classList.add('marked');
+  } else {
+    event.target.classList.add('marked');
+  }
+}
+
 function criaTarefa() {
   const olListaTarefas = document.getElementById('lista-tarefas');
   const tarefa = document.createElement('li');
 
   tarefa.innerText = input.value;
+  tarefa.className = 'tarefa';
+  tarefa.addEventListener('click', addBackground);
   olListaTarefas.appendChild(tarefa);
   input.value = '';
 }
